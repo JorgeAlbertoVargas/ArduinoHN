fdff74f484229e712228d79af890036a74c20b5b
Jorge Alberto Vargas <jorge.vargas@github.com>
2026-08-27 14:22:02 -0600

    fix: add default values for missing env variables in cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,14 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      shopifyDomain: process.env.SHOPIFY_DOMAIN,
-      shopifyToken: process.env.SHOPIFY_STOREFRONT_TOKEN,
-      nocodbUrl: process.env.NOCODB_URL,
-      nocodbToken: process.env.NOCODB_TOKEN,
-      nocodbProjectsTable: process.env.NOCODB_PROJECTS_TABLE,
-      nocodbOrdersTable: process.env.NOCODB_ORDERS_TABLE,
-      nocodbOrderItemsTable: process.env.NOCODB_ORDER_ITEMS_TABLE,
-      nocodbProductosTable: process.env.NOCODB_PRODUCTOS_TABLE
+      shopifyDomain: process.env.SHOPIFY_DOMAIN || 'arduinohn.myshopify.com',
+      shopifyToken: process.env.SHOPIFY_STOREFRONT_TOKEN || 'mock_storefront_token_12345',
+      nocodbUrl: process.env.NOCODB_URL || 'https://ianocodb.syteccorpia.com/',
+      nocodbToken: process.env.NOCODB_TOKEN || 'nc_pat_cTQfsJmbeIoKknRricm6Ch6iLXzcwGt3XlLBRVNh',
+      nocodbProjectsTable: process.env.NOCODB_PROJECTS_TABLE || 'movjxf98fu72rx8',
+      nocodbOrdersTable: process.env.NOCODB_ORDERS_TABLE || 'moet2c7py49b1cc',
+      nocodbOrderItemsTable: process.env.NOCODB_ORDER_ITEMS_TABLE || 'm0dqnot1yjfu8za',
+      nocodbProductosTable: process.env.NOCODB_PRODUCTOS_TABLE || 'mgl2cerr4iret4q'
     }
   },
   nitro: {
