2f665d3860ffbe918311cb1d5a7477162a63e590
Jorge Alberto Vargas <jorge.vargas@github.com>
2026-08-28 20:51:22 -0600

    fix: make auth middleware handler async

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler } from 'h3'
 import { getUserFromEvent } from '../utils/jwt'
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   // Populate event context with user if authenticated
   const user = await getUserFromEvent(event)
   if (user) {
